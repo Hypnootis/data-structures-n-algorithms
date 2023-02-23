fdd00052e0f4e21cdb3cba5aa15a598963591679
hypnootis <tm.pasanen@outlook.com>
2023-02-23 13:49:08 +0200

    Massively refactored

--- Exercises/1-Bitwise Operators/Ex1/Ex_1_1_a.java
+++ Exercises/1-Bitwise Operators/Ex1/Ex_1_1_a.java
@@ -1,4 +1,4 @@
-public class App {
+public class Ex_1_1_a {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ Exercises/2-Recursion/Ex1/Ex_2_1_a.java
@@ -0,0 +1,6 @@
+public class Ex_2_1_a {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
