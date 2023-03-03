2b954db6883910896447cb94108e99c37b33e8ac
hypnootis <tm.pasanen@outlook.com>
2023-03-03 13:55:07 +0200

    Project now works, including tests, in Eclipse

---
+++ src/main/java/bitwise/BitCounter.java
@@ -0,0 +1,15 @@
+package bitwise;
+
+public class BitCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
+	public int countBits() {
+		return 0;
+	}
+}
---
+++ src/test/java/dsa/BitCounterTest.java
@@ -0,0 +1,18 @@
+package dsa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import bitwise.BitCounter;
+
+class BitCounterTest {
+	
+	BitCounter bc = new BitCounter();
+	
+	@Test
+	void testCountBits() {
+		fail("Not yet implemented");
+	}
+
+}
