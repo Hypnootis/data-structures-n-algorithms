e7a27f22ba83e7223ba0cdd5f3cf5f7c73fa6da8
hypnootis <tm.pasanen@outlook.com>
2023-05-05 19:59:18 +0300

    Binary search done

---
+++ src/test/java/dsa/LinkedListsTest.java
@@ -0,0 +1,25 @@
+package dsa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import dsa.collections.LinkedLists;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class LinkedListsTest {
+    LinkedList<String> testListOne = new LinkedList<String>(Arrays.asList("Hello", "World", "!"));
+    LinkedList<String> testListTwo = new LinkedList<String>(Arrays.asList("This", "Is", "Text"));
+    @Test
+    void testConcatenateList() {
+        LinkedList<String> solution = new LinkedList<String>(Arrays.asList("Hello", "World", "!", "This", "Is", "Text"));
+        Assert.assertEquals(solution, LinkedLists.concatenateList(testListOne, testListTwo));
+    }    
+
+    @Test
+    void testMixList() {
+       LinkedList<String> solution = new LinkedList<String>(Arrays.asList("Hello", "This", "World", "Is", "!", "Text")); 
+       Assert.assertEquals(solution, LinkedLists.mixList(testListOne, testListTwo));
+    }
+}
